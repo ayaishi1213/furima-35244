6e05eeeb8330ccde41d95c65cc9f03f237ab774e
ayaishi1213 <shitianxuannai958@gmail.com>
2021-04-16 15:51:15 +0900

    商品情報編集機能_修正_1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :create, :edit]
+  before_action :authenticate_user!, only:[:new, :create, :edit, :update]
   before_action :set_params, only:[:show, :edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -23,9 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless @item.user == current_user
-      redirect_to root_path
-    end
   end
 
   def update
@@ -46,5 +44,11 @@ class ItemsController < ApplicationController
 
   def set_params
     @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    unless @item.user == current_user
+      redirect_to root_path
+    end
   end
 end
